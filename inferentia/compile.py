7a59aa5067a3c2c2f1e34e8cce758380546e70b7
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-09-08 17:32:57 -0700

    use the same import

--- inferentia/compile.py
+++ inferentia/compile.py
@@ -1,14 +1,15 @@
 import argparse
 import numpy as np
 import shutil
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 import tensorflow.neuron as tfn
+import tensorflow.compat.v1.keras as keras
 from resnet_model import ResNet50
 
 import os
 
-tf.keras.backend.set_learning_phase(0)
-tf.keras.backend.set_floatx('float16')
+keras.backend.set_learning_phase(0)
+keras.backend.set_floatx('float16')
 
 
 COMPILED_MODEL_DIR = 'compiled-keras-resnet50'
@@ -16,7 +17,7 @@ model = ResNet50(1000)
 model.load_weights('saved_weights.h5')
 print(model.inputs[0].dtype)
 shutil.rmtree('tmp-model', ignore_errors=True)
-tf.saved_model.simple_save(session=tf.keras.backend.get_session(), export_dir='tmp-model', inputs={'input': model.inputs[0]}, outputs={'output': model.outputs[0]})
+tf.saved_model.simple_save(session=keras.backend.get_session(), export_dir='tmp-model', inputs={'input': model.inputs[0]}, outputs={'output': model.outputs[0]})
 
 
 batch_sizes = [16]
